36357c168adb3fc907afefc1da78d2decc033aca
alpzaf <alialpzafer@gmail.com>
2022-01-04 14:27:27 +0300

    initial release

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,61 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Weather App</h1>
+    <div class="enter-city">
+      <input
+        type="text"
+        placeholder="Enter a city"
+        v-model="data.city"
+        @keyup.enter="getWeather"
+      />
+    </div>
+    <div class="weather">
+      <h1>6&deg;</h1>
+      <h2>Cloudy</h2>
+      <h3>Lots of Clouds</h3>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import { reactive } from "vue";
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  setup() {
+    let data = reactive({
+      city: "",
+      weather: null,
+    });
+
+    const getWeather = () => {
+      axios(
+        "https://api.openweathermap.org/data/2.5/weather?units=metric&q=istanbul&appid=66ba8a1b533a7a22400f6b465181d348"
+      ).then((response) => {
+        console.log(response.data);
+      });
+    };
+
+    return {
+      data,
+      getWeather,
+    };
   },
 };
 </script>
+
+<style scoped>
+.enter-city input {
+  font-size: 40px;
+}
+.weather {
+  margin-top: 10px;
+}
+.weather h1 {
+  font-size: 80px;
+}
+h1,
+h2,
+h3 {
+  margin-bottom: 0;
+}
+</style>
